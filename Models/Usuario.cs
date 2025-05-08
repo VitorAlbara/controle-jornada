7cc50cdd1cd290a21d05b5a62ea2ff6ff7716d1f
EDUARDO KAWAN FELICHAK <eduardo_felichak@estudante.sesisenai.org.br>
2025-05-08 20:03:25 -0300

    feat: criada model usuário

---
+++ Models/Usuario.cs
@@ -0,0 +1,14 @@
+﻿namespace controle_jornada.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string Login { get; set; } = string.Empty;
+        public string Nome { get; set; } = string.Empty;
+        public string Sobrenome { get; set; } = string.Empty;
+        public DateTime CriadoEm { get; set; }
+        public DateTime UltimoAcesso { get; set; }
+        public string ChaveApi { get; set; } = string.Empty;
+        public int ProjetoId { get; set; }
+    }
+}
