30cb02ce46d010fc43f4125012d468ca6ec80797
EDUARDO KAWAN FELICHAK <eduardo_felichak@estudante.sesisenai.org.br>
2025-05-08 20:18:22 -0300

    feat: criado helper do usuário

---
+++ Helpers/AppData/AppDataHelper.cs
@@ -0,0 +1,23 @@
+﻿namespace controle_jornada.Helpers.AppData
+{
+    public static class AppDataHelper
+    {
+        private static readonly string AppDataCaminho = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "controle-jornada"
+        );
+
+        public static string GetPastaAppData()
+        {
+            if (!Directory.Exists(AppDataCaminho))
+                Directory.CreateDirectory(AppDataCaminho);
+
+            return AppDataCaminho;
+        }
+
+        public static string GetArquivoUsuario()
+        {
+            return Path.Combine(GetPastaAppData(), "usuario.json");
+        }
+    }
+}
---
+++ Helpers/AppData/DadosUsuario.cs
@@ -0,0 +1,26 @@
+﻿using controle_jornada.Models;
+using Newtonsoft.Json;
+
+namespace controle_jornada.Helpers.AppData
+{
+    public static class DadosUsuario
+    {
+        public static void SalvarDadosUsuario(Usuario usuario)
+        {
+            string filePath = AppDataHelper.GetArquivoUsuario();
+            string json = JsonConvert.SerializeObject(usuario, Formatting.Indented);
+            File.WriteAllText(filePath, json);
+        }
+
+        public static Usuario CarregarDadosUsuario()
+        {
+            string filePath = AppDataHelper.GetArquivoUsuario();
+            if (File.Exists(filePath))
+            {
+                string jsonContent = File.ReadAllText(filePath);
+                return JsonConvert.DeserializeObject<Usuario>(jsonContent);
+            }
+            return null;
+        }
+    }
+}
