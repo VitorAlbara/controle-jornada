59cb328345128f969d4501a38b6102ad1d7900b8
Vitor Albara <vitoralbara1@gmail.com>
2025-05-22 20:47:04 -0300

    feat:criação model Versão

---
+++ Models/Versao.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace controle_jornada.Models
+{
+    [Table("versoes")]
+    public class Versao
+    {
+        [Key]
+        public int Id { get; set; }
+        [Key]
+        public int ProjetoId { get; set; }
+        public string Nome { get; set; }
+        public DateOnly DataInicio { get; set; }
+        public DateOnly DataVencimento { get; set; }
+        public Projeto Project { get; set; }
+
+        public ICollection<Task> Tarefas { get; set; }
+    }
+}
