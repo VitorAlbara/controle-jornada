8b01d35e3473dfdd141b6b5989ee6f2a89e17063
Vitor Albara <vitoralbara1@gmail.com>
2025-05-22 20:20:52 -0300

    feat: criação model EntradaLocal

---
+++ Models/EntradaLocal.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace controle_jornada.Models
+{
+    internal class EntradaLocal
+    {
+        [Table("local_entries")]
+        public class LocalEntrie
+        {
+            [Key]
+            public int Id { get; set; }
+            public string TarefaId { get; set; }
+            public int tarefaUsuarioId { get; set; }
+            public Task Tarefa { get; set; }
+            public DateOnly DataEntrada { get; set; }
+            public int Duracao { get; set; }
+        }
+    }
+}
