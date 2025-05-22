3a4cb3125fbef6ac035bc683433c2991c2c4af29
Vitor Albara <vitoralbara1@gmail.com>
2025-05-22 20:03:06 -0300

    feat: criação model de Entrada

---
+++ Models/Entrada.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace controle_jornada.Models
+{
+    [Table("entrada")]
+    public class Entrada
+    {
+        [Key]
+        public int Id { get; set; }
+        public string TarefaId { get; set; }
+        public int TarefaUsuarioId { get; set; }
+        public Task Tarefa { get; set; }
+        public DateOnly DataEntrada { get; set; }
+        public int Duracao { get; set; }
+    }
+}
