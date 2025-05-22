ba277691c52d83c5435d67d890b48fd3d75eae91
Vitor Albara <vitoralbara1@gmail.com>
2025-05-22 19:51:14 -0300

    feat: criação model AppVersao

---
+++ Models/AppVersao.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace controle_jornada.Models
+{
+        [Table("app_versao")]
+        public class AppVersao
+        {
+            [Key]
+            public Guid Id { get; set; } = new Guid();
+            public string Nome { get; set; }
+        }
+    }
