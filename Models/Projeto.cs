84ac6f904a93c3b5812d968022bf79d6131ea0fe
Vitor Albara <vitoralbara1@gmail.com>
2025-05-22 20:31:28 -0300

    feat: criação model Projetos

---
+++ Models/Projeto.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace controle_jornada.Models
+{
+    [Table("projetos")]
+    public class Projeto
+    {
+        [Key]
+        public int Id { get; set; }
+        public string Nome { get; set; }
+
+        public ICollection<Version> Versoes { get; set; }
+    }
+}
