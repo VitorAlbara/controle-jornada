028f620ace53f8411b25ca93a039ffb2c8aa163a
Vitor Albara <vitoralbara1@gmail.com>
2025-06-02 19:43:42 -0300

    feat:Criação Dtos

---
+++ DTOs/TempoEntradaResultDto.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+
+namespace controle_jornada.DTOs
+{
+    internal class TempoEntradaResultDto
+    {
+        [JsonPropertyName("time_entries")]
+        public List<TempoEntradaDto> TempoEntradas { get; set; }
+    }
+}
