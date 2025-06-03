b00514105854c9848843446b53c11f451b318eda
Eduardo Felichak <100370989+EduardoFelichak@users.noreply.github.com>
2025-06-02 22:33:50 -0300

    fix: ajuste no repositório de entradas

--- Repositories/EntradaRepo.cs
+++ Repositories/EntradaRepo.cs
@@ -9,159 +9,148 @@ namespace controle_jornada.Repositories
 {
     public class EntradaRepo
     {
-        private readonly ContextoBanco _context = new ContextoBanco();
-
-        public async System.Threading.Tasks.Task AddOrUpdateFromRedmine(List<Entrada> redmineEntries)
+        private readonly ContextoBanco _contexto = new ContextoBanco();
+        private readonly Usuario _usuario = DadosUsuario.CarregarDadosUsuario();
+        public async Task AdicionarOuAtualizarDoRedmine(List<Entrada> entradasRedmine)
         {
-            foreach (var entry in redmineEntries)
+            foreach (var entrada in entradasRedmine)
             {
-                var existingEntry = await _context.Entradas.FirstOrDefaultAsync(e => e.Id == entry.Id);
+                var entradaExistente = await _contexto.Entradas.FirstOrDefaultAsync(e => e.Id == entrada.Id);
 
-                if (existingEntry == null)
-                    await _context.Entradas.AddAsync(entry);
+                if (entradaExistente == null)
+                    await _contexto.Entradas.AddAsync(entrada);
                 else
                 {
-                    existingEntry.Duracao = entry.Duracao;
-                    existingEntry.DataEntrada = entry.DataEntrada;
-                    existingEntry.TarefaId = entry.TarefaId;
-                    _context.Entradas.Update(existingEntry);
+                    entradaExistente.Duracao     = entrada.Duracao;
+                    entradaExistente.DataEntrada = entrada.DataEntrada;
+                    entradaExistente.TarefaId    = entrada.TarefaId;
+                    _contexto.Entradas.Update(entradaExistente);
                 }
             }
 
-            await _context.SaveChangesAsync();
+            await _contexto.SaveChangesAsync();
         }
 
-        public async Task<int> GetNextNegativeIdAsync()
+        public async Task<int> PegarProxIdNegativo()
         {
-            int? smallestNegativeId = await _context.Entradas
+            int? menorIdNegativo = await _contexto.Entradas
                 .Where(e => e.Id < 0)
                 .OrderBy(e => e.Id)
                 .Select(e => (int?)e.Id)
                 .FirstOrDefaultAsync();
 
-            if (smallestNegativeId.HasValue)
-                return smallestNegativeId.Value - 1;
+            if (menorIdNegativo.HasValue)
+                return menorIdNegativo.Value - 1;
             else
                 return -1;
         }
 
-        public async Task<List<Entrada>> GetEntriesByUserAndDate(DateOnly date)
+        public async Task<List<Entrada>> PegarTodosPorData(DateOnly data)
         {
-            var user = DadosUsuario.CarregarDadosUsuario();
-
-            return await _context.Entradas
+            return await _contexto.Entradas
                 .Include(e => e.Tarefa)
-                .Where(e => e.TarefaUsuarioId == user.Id && e.DataEntrada == date)
+                .Where(e => e.TarefaUsuarioId == _usuario.Id && e.DataEntrada == data)
                 .ToListAsync();
         }
 
-        public async Task<int> GetTotalTimeByTaskAndDate(int tarefaId, DateOnly date)
+        public async Task<int> PegarTotalTempoTarefa(int tarefaId, DateOnly data)
         {
-            var user = DadosUsuario.CarregarDadosUsuario();
-
-            return await _context.Entradas
+            return await _contexto.Entradas
                 .Include(e => e.Tarefa)
-                .Where(e => e.TarefaUsuarioId == user.Id
+                .Where(e => e.TarefaUsuarioId == _usuario.Id
                         && e.TarefaId == tarefaId
-                        && e.DataEntrada == date)
+                        && e.DataEntrada == data)
                 .SumAsync(e => e.Duracao);
         }
 
-        public async System.Threading.Tasks.Task Add(Entrada entry)
+        public async Task AdicionarOuAtualizar(Entrada entrada)
         {
-            var existingEntry = await _context.Entradas.FindAsync(entry.Id);
+            var entradaExistente = await _contexto.Entradas.FindAsync(entrada.Id);
 
-            if (existingEntry != null)
+            if (entradaExistente != null)
             {
-                existingEntry.Duracao = entry.Duracao;
-                _context.Entradas.Update(existingEntry);
+                entradaExistente.Duracao = entrada.Duracao;
+                _contexto.Entradas.Update(entradaExistente);
             }
             else
-                _context.Entradas.Add(entry);
+                _contexto.Entradas.Add(entrada);
 
-            await _context.SaveChangesAsync();
+            await _contexto.SaveChangesAsync();
         }
 
-        public async Task<int> GetRealesedTime(DateOnly date)
+        public async Task<int> PegarTempoLancado(DateOnly data)
         {
-            var user = DadosUsuario.CarregarDadosUsuario();
-
-            return await _context.Entradas
-                .Where(e => e.DataEntrada == date && e.TarefaUsuarioId == user.Id)
+            return await _contexto.Entradas
+                .Where(e => e.DataEntrada == data && e.TarefaUsuarioId == _usuario.Id)
                 .SumAsync(e => e.Duracao);
         }
 
-        public async Task<int> GetTotalSpentTimePerDate(DateOnly date)
+        public async Task<int> PegarTotalGastoDia(DateOnly data)
         {
-            var user = DadosUsuario.CarregarDadosUsuario();
-
-            return await _context.Entradas
-                .Where(e => e.DataEntrada == date && e.TarefaUsuarioId == user.Id)
+            return await _contexto.Entradas
+                .Where(e => e.DataEntrada == data && e.TarefaUsuarioId == _usuario.Id)
                 .SumAsync(e => e.Duracao) +
-                await _context.EntradasLocais
-                .Where(e => e.DataEntrada == date && e.TarefaUsuarioId == user.Id)
+                await _contexto.EntradasLocais
+                .Where(e => e.DataEntrada == data && e.TarefaUsuarioId == _usuario.Id)
                 .SumAsync(e => e.Duracao);
         }
 
-        public async Task<int> GetWorkTimeSpentPerDate(DateOnly date)
+        public async Task<int> PegarTotalTrabalhadoDia(DateOnly data)
         {
-            var user = DadosUsuario.CarregarDadosUsuario();
-
-            return await _context.Entradas
-                .Where(e => e.DataEntrada == date && e.TarefaUsuarioId == user.Id
-                        && e.TarefaId != "Estudo")
-                .SumAsync(e => e.Duration) +
-                await _context.EntradasLocais
-                .Where(e => e.DataEntrada == date && e.TarefaUsuarioId == user.Id
-                        && e.TarefaId != "Estudo")
-                .SumAsync(e => e.Duration);
-        }
-
-        public async Task<int> GetStudyTimeSpentPerDate(DateOnly date)
-        {
-            var user = DadosUsuario.CarregarDadosUsuario();
-
-            return await _context.Entradas
-                .Where(e => e.DataEntrada == date && e.TarefaUsuarioId == user.Id
-                        && e.TarefaId == "Estudo")
-                .SumAsync(e => e.Duration) +
-                await _context.EntradasLocais
-                .Where(e => e.DataEntrada == date && e.TarefaUsuarioId == user.Id
-                        && e.TarefaId == "Estudo")
-                .SumAsync(e => e.Duration);
-        }
-
-        public async Task<int> GetTotalSpentTimeByTask(int taskId)
-        {
-            var user = DadosUsuario.CarregarDadosUsuario();
-            return await _context.Entradas
-                .Where(e => e.TarefaId == taskId && e.TarefaUsuarioId == user.Id)
+            return await _contexto.Entradas
+                .Where(e => e.DataEntrada == data && e.TarefaUsuarioId == _usuario.Id
+                        && e.TarefaId != -1)
                 .SumAsync(e => e.Duracao) +
-                await _context.EntradasLocais
-                .Where(e => e.TarefaId == taskId && e.TarefaUsuarioId == user.Id)
+                await _contexto.EntradasLocais
+                .Where(e => e.DataEntrada == data && e.TarefaUsuarioId == _usuario.Id
+                        && e.TarefaId != -1)
                 .SumAsync(e => e.Duracao);
         }
 
-        public async System.Threading.Tasks.Task Update(Entrada entrie)
+        public async Task<int> PegarTotalEstudadoDia(DateOnly data)
         {
-            _context.Entradas.Update(entrie);
-            await _context.SaveChangesAsync();
+            var user = DadosUsuario.CarregarDadosUsuario();
+
+            return await _contexto.Entradas
+                .Where(e => e.DataEntrada == data && e.TarefaUsuarioId == _usuario.Id
+                        && e.TarefaId == -1)
+                .SumAsync(e => e.Duracao) +
+                await _contexto.EntradasLocais
+                .Where(e => e.DataEntrada == data && e.TarefaUsuarioId == _usuario.Id
+                        && e.TarefaId == -1)
+                .SumAsync(e => e.Duracao);
         }
 
-        public async System.Threading.Tasks.Task Delete(int id)
+        public async Task<int> PegarTotalTempoGastoTarefa(int tarefaId)
         {
-            var entrie = await _context.Entradas.FirstOrDefaultAsync(e => e.Id == id);
+            return await _contexto.Entradas
+                .Where(e => e.TarefaId == tarefaId && e.TarefaUsuarioId == _usuario.Id)
+                .SumAsync(e => e.Duracao) +
+                await _contexto.EntradasLocais
+                .Where(e => e.TarefaId == tarefaId && e.TarefaUsuarioId == _usuario.Id)
+                .SumAsync(e => e.Duracao);
+        }
 
-            if (entrie != null)
+        public async Task Atualizar(Entrada entrada)
+        {
+            _contexto.Entradas.Update(entrada);
+            await _contexto.SaveChangesAsync();
+        }
+
+        public async Task Deletar(int id)
+        {
+            var entrada = await _contexto.Entradas.FirstOrDefaultAsync(e => e.Id == id);
+
+            if (entrada != null)
             {
-                _context.Entradas.Remove(entrie);
-                await _context.SaveChangesAsync();
+                _contexto.Entradas.Remove(entrada);
+                await _contexto.SaveChangesAsync();
             }
         }
 
-        public async Task<Entrada?> FindByIdAsync(int id)
+        public async Task<Entrada?> PegarPorId(int id)
         {
-            return await _context.Entradas.FindAsync(id);
+            return await _contexto.Entradas.FindAsync(id);
         }
     }
 }
