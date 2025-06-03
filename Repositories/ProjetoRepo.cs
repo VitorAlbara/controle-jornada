35ac21b040169ed68dbcb448b17ca27f05194f55
Vitor Albara <vitoralbara1@gmail.com>
2025-06-02 22:21:54 -0300

    feat: criação Projeto Repo

---
+++ Repositories/ProjetoRepo.cs
@@ -0,0 +1,17 @@
+﻿using System.Windows.Forms;
+using controle_jornada.Infra.Context;
+using controle_jornada.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace controle_jornada.Repositories
+{
+    public class ProjetoRepo
+    {
+        private readonly ContextoBanco _context = new ContextoBanco();
+
+        public async Task<List<Projeto>> GetAll()
+        {
+            return await _context.Projetos.Where(p => p.Id != 0).ToListAsync();
+        }
+    }
+}
