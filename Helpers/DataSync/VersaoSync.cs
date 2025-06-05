5d752c3d590d5714fbc891201d25e83e9e353bf3
EduardoFelichak <edukf90@gmail.com>
2025-06-04 21:16:53 -0300

    feat: Criados os helpers de sincronização

---
+++ Helpers/DataSync/VersaoSync.cs
@@ -0,0 +1,32 @@
+﻿using controle_jornada.Models;
+using controle_jornada.Repositories;
+using controle_jornada.Services;
+
+namespace controle_jornada.Helpers.DataSync
+{
+    public class VersaoSync
+    {
+        public static async Task Run(DateOnly data)
+        {
+            RedmineServ redmineServ = new RedmineServ();
+            VersaoRepo versaoRepo   = new VersaoRepo();
+
+            Versao? versao = await redmineServ.PegarVersaoAtual(data);
+
+            if (versao == null) 
+                throw new Exception("Nenhuma versão encontrada.");
+
+            Versao? versaoExistente = await versaoRepo.PegarPeloId(versao.Id);
+
+            if (versaoExistente == null)
+                versaoRepo.Adicionar(versao);
+            else
+            {   
+                versaoExistente.DataInicio     = versao.DataInicio;
+                versaoExistente.DataVencimento = versao.DataVencimento;
+
+                await versaoRepo.Atualizar(versaoExistente);
+            }
+        }
+    }
+}
